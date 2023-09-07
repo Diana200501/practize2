2926e3a5220a1838be86a6e10793c72c3816606c
KDiana! <137039795+Diana200501@users.noreply.github.com>
2023-09-07 17:35:11 +0300

    Add files via upload

---
+++ Practiceeee2/src/main/kotlin/Main.kt
@@ -0,0 +1,64 @@
+fun main(args: Array<String>) {
+  //задание 1
+   val age1 = 42.0
+    val age2 = 21.0
+    //____
+
+    //задание 2
+    val avg1 = (age1 + age2)/2
+    //___
+
+    //задание 4
+    val firstName: String = "Diana"
+    val lastName: String = "Kosheva"
+    //____
+
+    //5
+    val fullName: String = firstName + " " + lastName
+    println(fullName)
+
+ //6
+ val myDetails: String = "Привет, меня зовут $fullName"
+ println(myDetails)
+
+ //7
+ val test: Triple <Int, Int, Int> = Triple(10,20,2004)
+
+ //8
+ val month = test.first
+ val day = test.second
+ val year = test.third
+
+ //9
+ val (a,_,c) = test
+
+ //10
+val test2: Triple <Int, Int, Int> = Triple(10,20,2004)
+ val Month = test2.first + 5
+ val Day = test2.second
+ val Year = test2.third
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
